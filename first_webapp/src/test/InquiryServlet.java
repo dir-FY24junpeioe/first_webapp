7eac2233121c537e9bb82c1e5ae2a6c065400d73
junpeioe <junpei.oe.g070@dirbato.co.jp>
2024-04-18 14:54:25 +0900

    webapp13

--- first_webapp/src/test/InquiryServlet.java
+++ first_webapp/src/test/InquiryServlet.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,11 +33,30 @@ public class InquiryServlet extends HttpServlet {
 	    var usermail = request.getParameter("usermail");
 	    var content = request.getParameter("content");
 	    
-	    // JSPに値を送る
-	    request.setAttribute("username" , username);
-	    request.setAttribute("usermail" , usermail);
-	    request.setAttribute("content" , content);
-	    
+	 // 入力項目チェック（バリデーション）
+        List<String> errors = new ArrayList<String>();
+
+        if(username == null || username.equals("")) {  /* 氏名 */
+            errors.add("氏名を入力してください");
+        }
+        if(usermail == null || usermail.equals("")) {  /* メールアドレス */
+            errors.add("メールアドレスを入力してください");
+        }
+        if(content == null || content.equals("")) {    /* お問い合わせ内容 */
+            errors.add("お問い合わせ内容を入力してください");
+        }
+
+        // 入力内容にエラーがあったかどうか
+        if(errors.size() > 0) {
+            // JSPにエラー内容を送る
+            request.setAttribute("errors", errors);
+        } else {
+            // JSPに入力データを送る
+            request.setAttribute("username", username);
+            request.setAttribute("usermail", usermail);
+            request.setAttribute("content", content);
+        }
+	      
 	    // ビューとなるJSPを指定して表示する
 	    var rd = request.getRequestDispatcher("/WEB-INF/inquiry_result.jsp");
 	    rd.forward(request, response);
